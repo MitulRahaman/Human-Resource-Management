eb97f7b79ac86a466ed5975b9e5b36b1f8037a0d
RabeyaBosriTamanna <rbtamanna@appnap.io>
2023-11-23 13:53:39 +0600

    feat: Authorization trait

---
+++ app/Repositories/AuthorizationRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Degree;
+use Illuminate\Support\Facades\DB;
+
+class AuthorizationRepository
+{
+
+}
---
+++ app/Traits/AuthorizationTrait.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Traits;
+
+trait AuthorizationTrait {
+
+    public function __construct()
+    {
+
+    }
+}

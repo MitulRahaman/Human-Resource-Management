0836134125218f15ea20decdd5ef80a73f9cbe55
MitulRahman <mitul.rahman@appnap.io>
2023-12-03 09:33:19 +0600

    even trigerred after leave applied successfully

--- app/Http/Controllers/LeaveApply/LeaveApplyController.php
+++ app/Http/Controllers/LeaveApply/LeaveApplyController.php
@@ -90,19 +90,15 @@ class LeaveApplyController extends Controller
     }
     public function store(LeaveApplyAddRequest $request)
     {
-        //abort_if(!$this->setSlug('applyLeave')->checkAuthorization(), 403, 'You don\'t have permission!');
+        abort_if(!$this->setSlug('applyLeave')->checkAuthorization(), 403, 'You don\'t have permission!');
         try {
-                if($this->leaveApplyService->storeLeaves($request)) {
-                    return redirect('leaveApply/manage')->with('success', 'Leave application submitted successfully.');
-                } else {
-                    return redirect('leaveApply/apply')->with('error', $response);
-                }
-                if(event(new LeaveApplied($request))) {
-
-                }
-             else {
-                return redirect('leaveApply/apply')->with('error', "Currently no HR is assigned in your branch");
+            if($this->leaveApplyService->storeLeaves($request)) {
+                event(new LeaveApplied($request));
+                return redirect('leaveApply/manage')->with('success', 'Leave application submitted successfully.');
+            } else {
+                return redirect('leaveApply/apply')->with('error', $response);
             }
+
         } catch (\Exception $exception) {
             return redirect()->back()->with('error', $exception->getMessage());
         }

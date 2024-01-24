b80691adf6d44377962d016f75c685e657a73d11
MitulRahaman <Mitul.unknown@gmail.com>
2024-01-25 00:47:50 +0600

    completed

---
+++ README.md
@@ -0,0 +1,30 @@
+# RAS Assignment
+
+This is an admin panel project.
+
+##########
+Installation
+##########
+
+1. Clone the repository.
+
+2. Run `composer install`
+
+3. Run `cp .env.example .env`
+
+4. Update database configuration
+
+5. Run `php artisan migrate`
+
+6. Run `php artisan db:seed`
+
+7. Run `php artisan key:generate`
+
+8. Run `php artisan serve`
+
+##########
+
+Version 1.0
+=============
+
+Release Date: N/A
---
+++ app/Http/Middleware/Log.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class Log
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $temp = $request->all();
+        $start = microtime(true);
+        $response = $next($request);
+        $end = microtime(true);
+        $duration = ($end - $start) * 1000;
+        $responseJson =$response? json_encode($response):null;
+        $headersJson =$request->header()? json_encode($request->header()):null;
+        if(array_key_exists('photo', $temp)) {
+            unset($temp['photo']);
+            $paramsJson =$temp ? json_encode($temp):null;
+        } else {
+            $paramsJson =$request->all() ? json_encode($request->all()):null;
+        }
+        $uri = $request->getRequestUri();
+        if($uri=="/login")
+            $paramsJson='';
+        $log = DB::table('logs')
+            ->insert([
+                'uri'=>$uri,
+                'method'=>$request->getMethod(),
+                'user_id'=>auth()->user() ? auth()->user()->id : null,
+                'header' => $headersJson,
+                'params'=>$paramsJson,
+                'duration' => $duration,
+                'ip_address'=>$request->ip(),
+                'user_agent'=>$request->header('user-agent'),
+                'response_code'=> $response ? $response->getStatusCode() : null,
+                'response'=> $responseJson,
+                'created_at'=>date('Y-m-d H:i:s'),
+            ]);
+        return $response;
+    }
+}
